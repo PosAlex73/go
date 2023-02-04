2958167825696685f2c1b81af479d4e52c9517b6
alexpos <alternativa988@inbox.ru>
2023-02-05 03:32:08 +0400

    add front

---
+++ resources/views/parts/scripts.blade.php
@@ -0,0 +1,9 @@
+<script src="{{ asset('js/assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('js/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('js/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('js/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('js/assets/libs/node-waves/waves.min.js') }}"></script>
+<script src="{{ asset('js/assets/libs/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
+
+<!-- App js -->
+<script src="{{ asset('js/assets/js/app.js') }}"></script>
