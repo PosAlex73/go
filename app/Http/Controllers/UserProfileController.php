c8fa3ad933bf9acd5a7675c87c5c65bd4ee35c64
alexpos <alternativa988@inbox.ru>
2023-02-05 22:37:41 +0400

    add user profile

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreUserProfileRequest;
 use App\Http\Requests\UpdateUserProfileRequest;
 use App\Models\UserProfile;
+use Illuminate\Support\Facades\Auth;
 
 class UserProfileController extends Controller
 {
     public function index()
     {
+        $user = Auth::user();
+        $progress = $user->progress;
+        $user_settings = $user->settings;
+
         return view('front.profile.index');
     }
 }
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -37,31 +37,19 @@
                 </div>
                 <!-- end page title -->
 
-
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 mt-5">
                         @yield('content')
                     </div>
                 </div>
 
+
             </div>
+
             <!-- End Page-content -->
 
-            <footer class="footer">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <script>document.write(new Date().getFullYear())</script> &copy; Qovex.
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="text-sm-end d-none d-sm-block">
-                                Design & Develop by Themesbrand
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </footer>
         </div>
+
         <!-- end main content-->
 
     </div>
@@ -70,54 +58,7 @@
 </div>
 <!-- end container-fluid -->
 
-<!-- Right Sidebar -->
-<div class="right-bar">
-    <div data-simplebar class="h-100">
-        <div class="rightbar-title px-3 py-4">
-            <a href="javascript:void(0);" class="right-bar-toggle float-end">
-                <i class="mdi mdi-close noti-icon"></i>
-            </a>
-            <h5 class="m-0">Settings</h5>
-        </div>
 
-        <!-- Settings -->
-        <hr class="mt-0" />
-        <h6 class="text-center mb-0">Choose Layouts</h6>
-
-        <div class="p-4">
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-1.jpg" class="img-thumbnail" alt="">
-            </div>
-
-            <div class="form-check form-switch mb-3">
-                <input type="checkbox" class="form-check-input theme-choice" id="light-mode-switch" checked />
-                <label class="form-check-label" for="light-mode-switch">Light Mode</label>
-            </div>
-
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-2.jpg" class="img-thumbnail" alt="">
-            </div>
-
-            <div class="form-check form-switch mb-3">
-                <input type="checkbox" class="form-check-input theme-choice" id="dark-mode-switch"
-                       data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css" />
-                <label class="form-check-label" for="dark-mode-switch">Dark Mode</label>
-            </div>
-
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-3.jpg" class="img-thumbnail" alt="">
-            </div>
-            <div class="form-check form-switch mb-5">
-                <input type="checkbox" class="form-check-input theme-choice" id="rtl-mode-switch"
-                       data-appStyle="assets/css/app-rtl.min.css" />
-                <label class="form-check-label" for="rtl-mode-switch">RTL Mode</label>
-            </div>
-
-        </div>
-
-    </div>
-    <!-- end slimscroll-menu-->
-</div>
 <!-- /Right-bar -->
 
 <!-- Right bar overlay-->
